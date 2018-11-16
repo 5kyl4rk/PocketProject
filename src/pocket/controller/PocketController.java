6a4f06b4c7117b6f2a99bdfc317d40328bdde396
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-11-16 12:25:45 -0700

    imported ImageIcon and made an array for PocketMonster

--- src/pocket/controller/PocketController.java
+++ src/pocket/controller/PocketController.java
@@ -1,5 +1,7 @@
 package pocket.controller;
+
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 import java.util.ArrayList;
 import pocket.model.PocketMonster;;
 
@@ -9,11 +11,18 @@ public class PocketController
 	private PocketMonster squirtle;
 	private PocketMonster charizard;
 	
+	private PocketMonster[] pocketArray;
+	private ImageIcon[] icons;
+	
+	
 	public PocketController()
 	{
 		pikachu = new PocketMonster();
 		squirtle = new PocketMonster("Squirtle", "Water", 14, "SquirtleSquad");
 		charizard = new PocketMonster("Charizard", "Fire - Flying", 60, "Burnt Shakes");
+		
+		icons = new ImageIcon[6];
+		pocketArray = new PocketMonster[6];
 	}
 	
 	public void start()
